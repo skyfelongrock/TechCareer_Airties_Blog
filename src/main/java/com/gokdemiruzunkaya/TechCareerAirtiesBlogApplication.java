f94d53519fca3d73c38b9a4fb475f29f7c16d994
skyfelongrock <skyfelongrock@gmail.com>
2022-10-31 05:07:28 +0300

    revize base

--- src/main/java/com/gokdemiruzunkaya/TechCareerAirtiesBlogApplication.java
+++ src/main/java/com/gokdemiruzunkaya/TechCareerAirtiesBlogApplication.java
@@ -2,12 +2,67 @@ package com.gokdemiruzunkaya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
+// Mongo aktif etmek icin
+// @EnableMongoRepositories
+
+//Aspect aktif etmek icin
+//@EnableAspectJAutoProxy(proxyTargetClass = true)
+
+//Asenkron açmak icin
+//@EnableAsync
+
+//Spring Boot Cache mekanizmasını aktif ediyorum
+//@EnableCaching
+
+//spring boot cache aktif ediyorum
+//redis: caching sağlamak
+//@EnableCaching
+
+//Asenkron açmak
+//@EnableAsync
+
+//React
+//GraphQL: Gereksiz API taşınmasını engeller API: username,password,data,special,longTime,
+//Jackson: JSON
+
+//Auditor
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
+
+//exclude:dahil etmemek
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        //org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        //org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+//@SpringBootApplication
 public class TechCareerAirtiesBlogApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TechCareerAirtiesBlogApplication.class, args);
+    /*
+    @Async
+    public void dataSupport(){
+    }*/
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
     }
 
+    public static void main(String[] args) {
+        //devtool active inactive
+        System.setProperty("spring.devtools.restart.enabled","true");
+
+        //Disables headless JOptionPane
+        System.setProperty("java.awt.headless", "false");
+
+        //PSVM
+        SpringApplication.run(TechCareerAirtiesBlogApplication.class, args);
+    }
 }
