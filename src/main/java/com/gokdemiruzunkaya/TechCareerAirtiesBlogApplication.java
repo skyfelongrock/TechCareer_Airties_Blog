7607a3bb7c5525e03f91f7232da7b5969e3fc58b
skyfelongrock <skyfelongrock@gmail.com>
2022-10-31 05:05:08 +0300

    Initial commit

---
+++ src/main/java/com/gokdemiruzunkaya/TechCareerAirtiesBlogApplication.java
@@ -0,0 +1,13 @@
+package com.gokdemiruzunkaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerAirtiesBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCareerAirtiesBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gokdemiruzunkaya/TechCareerAirtiesBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokdemiruzunkaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerAirtiesBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
